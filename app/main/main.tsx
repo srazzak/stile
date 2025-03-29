4353d0a7d37582e0502df91a2c212a49151e1203
Suhail Razzak <suhail.razzak@gmail.com>
2025-03-28 22:12:42 -0400

    rename welcome to main

--- app/main/main.tsx
+++ app/main/main.tsx
@@ -4,7 +4,7 @@ import { TodoList } from "@/components/todo/todo-list";
 import { todoStore } from "@/lib/storage";
 import { useLiveQuery } from "dexie-react-hooks";
 
-export function Welcome() {
+export function Main() {
   const todos = useLiveQuery(() => todoStore.getAllTodos(), [], []);
 
   return (
