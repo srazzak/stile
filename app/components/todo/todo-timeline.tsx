14932d93b57ea124e070997160e6f7deb660e321
Suhail Razzak <suhail.razzak@gmail.com>
2025-04-11 22:49:22 -0400

    Refactor todo timeline to use fixed time groups

--- app/components/todo/todo-timeline.tsx
+++ app/components/todo/todo-timeline.tsx
@@ -1,32 +1,14 @@
 import { EmptyTodo } from "@/components/todo/empty-todo";
 import { TodoList } from "@/components/todo/todo-list";
 import type { Todo } from "@/lib/storage/types";
+import { subDays } from "date-fns";
 
 export interface TodoTimlineProps {
   todos: Todo[];
 }
 
 export function TodoTimeline({ todos }: TodoTimlineProps) {
-  const grouped = todos.reduce<Record<string, Todo[]>>((acc, todo) => {
-    const originalDate = todo.completed
-      ? todo.completedAt
-      : new Date().toLocaleDateString("en-US");
-    const date = new Date(originalDate);
-    date.setHours(0, 0, 0, 0);
-    const dateString = date.toLocaleDateString("en-US", {
-      weekday: "short",
-      month: "short",
-      day: "numeric",
-    });
-
-    if (!acc[dateString]) {
-      acc[dateString] = [];
-    }
-
-    acc[dateString].push(todo);
-
-    return acc;
-  }, {});
+  const grouped = groupTodos(todos);
 
   const sortedDateArray = Object.entries(grouped).sort(([dateA], [dateB]) => {
     return new Date(dateB).getTime() - new Date(dateA).getTime();
@@ -46,4 +28,37 @@ export function TodoTimeline({ todos }: TodoTimlineProps) {
       {sortedDateArray.length === 0 ? <EmptyTodo /> : null}
     </section>
   );
+}
+
+type TimelineGroup =
+  | "Today"
+  | "Yesterday"
+  | "Previous 7 days"
+  | "Previous 30 days";
+
+function groupTodos(todos: Todo[]) {
+  const grouped: Record<TimelineGroup, Todo[]> = {
+    Today: [],
+    Yesterday: [],
+    "Previous 7 days": [],
+    "Previous 30 days": [],
+  };
+
+  todos.forEach((todo) => {
+    if (todo.completed) {
+      const today = new Date();
+
+      if (todo.completedAt > subDays(today, 1)) {
+        grouped["Yesterday"].push(todo);
+      } else if (todo.completedAt > subDays(today, 7)) {
+        grouped["Previous 7 days"].push(todo);
+      } else if (todo.completedAt > subDays(today, 30)) {
+        grouped["Previous 30 days"].push(todo);
+      }
+    } else {
+      grouped["Today"].push(todo);
+    }
+  });
+
+  return grouped;
 }
