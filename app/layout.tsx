e5ff923b294f75443daec54466fc136dfef22c0e
Suhail Razzak <suhail.razzak@gmail.com>
2025-05-06 19:59:24 -0400

    remove debug

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,31 +25,7 @@ export default function Layout() {
 
   return (
     <main className="mx-auto flex h-full w-full max-w-4xl flex-col p-4 pt-32 xl:max-w-3xl">
-      <div className="h-full w-full">
-        <div className="flex flex-col gap-2">
-          <span className="text-sm mt-16 text-gray-400">
-            active context: {activeContext}
-          </span>
-          <div className="inline-flex gap-2">
-            {keyBuffer.current.map((k) => (
-              <span key={k} className="text-sm bg-gray-300 px-1 py-0.5">
-                {k}
-              </span>
-            ))}
-          </div>
-          <div className="inline-flex gap-2">
-            {activeShortcuts.map((sh) => (
-              <span
-                key={sh.description}
-                className="text-sm bg-gray-300 px-1 py-0.5"
-              >
-                {sh.key}
-              </span>
-            ))}
-          </div>
-        </div>
-        <Outlet />
-      </div>
+      <Outlet />
     </main>
   );
 }
