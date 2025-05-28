08256e34dec7a9ff6c1bf077c2e3c2a5ba3e4958
Suhail Razzak <suhail.razzak@gmail.com>
2025-05-28 15:40:42 -0400

    remove section components

--- app/lib/storage/index.ts
+++ app/lib/storage/index.ts
@@ -1,13 +1,7 @@
 import { TodoDb } from "./TodoDatabase";
 
-declare global {
-  export interface ProcessEnv {
-    NEXT_PUBLIC_SUPABASE_URL: string;
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
-  }
-}
-
 export const todoStore = new TodoDb();
 
-// Initialize both services
-todoStore.initialize();
+if (typeof window !== "undefined") {
+  todoStore.initialize();
+}
