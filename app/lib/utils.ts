31f93f3614f0d3e1d2cf5f3accf771ab2e38cf05
Suhail Razzak <suhail.razzak@gmail.com>
2025-05-31 22:19:04 -0400

    Apply code formatting to utils

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,9 +1,12 @@
-import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-import { customAlphabet } from 'nanoid';
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { customAlphabet } from "nanoid";
 
-export const generateId = customAlphabet("123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz", 16);
+export const generateId = customAlphabet(
+  "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz",
+  16,
+);
 
 export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
