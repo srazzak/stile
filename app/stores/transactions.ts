082f14846cbf1796263e4709546e50087dc44b18
Suhail Razzak <suhail.razzak@gmail.com>
2025-05-31 21:43:24 -0400

    add zustand store

---
+++ app/stores/transactions.ts
@@ -0,0 +1,72 @@
+import type { Todo } from "@/lib/storage/types";
+import { create } from "zustand";
+
+export type DbTransaction =
+  | CreateTransaction
+  | DeleteTransaction
+  | UpdateTransaction;
+
+export interface CreateTransaction {
+  event: "create";
+  todoId: string;
+}
+
+export interface DeleteTransaction {
+  event: "delete";
+  todoId: string;
+}
+
+export interface UpdateTransaction {
+  event: "update";
+  todoId: string;
+  diffs: TodoUpdateDiff[];
+}
+
+interface FieldUpdateDiff<T, K extends keyof T> {
+  field: K;
+  previousValue: T[K];
+  newValue: T[K];
+}
+
+export type TodoUpdateDiff = {
+  [K in keyof Todo]: FieldUpdateDiff<Todo, K>;
+}[keyof Todo];
+
+interface DbTransactionStore {
+  transactions: DbTransaction[];
+  offset: number;
+  addTransaction: (transaction: DbTransaction) => void;
+  increaseOffset: () => void;
+  decreaseOffset: () => void;
+}
+
+/**
+ * Zustand store for managing database transactions history for undo/redo functionality.
+ * Stores a list of transactions and tracks the current state index.
+ */
+export const useTransactionStore = create<DbTransactionStore>()((set, get) => ({
+  transactions: [],
+  offset: -1,
+  addTransaction: (tx: DbTransaction) => {
+    const { transactions, offset: currentTransaction } = get();
+
+    // TODO: add max size for tx store
+    const newTransactions = transactions.slice(0, currentTransaction + 1);
+    newTransactions.push(tx);
+
+    set({
+      transactions: newTransactions,
+      offset: newTransactions.length - 1,
+    });
+  },
+  increaseOffset: () =>
+    set((state) => ({
+      offset: Math.max(-1, state.offset - 1),
+    })),
+  decreaseOffset: () =>
+    set((state) => ({
+      offset: Math.max(-1, state.offset + 1),
+    })),
+}));
+
+export const transactionStore = useTransactionStore;
