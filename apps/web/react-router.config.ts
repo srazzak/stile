a33459f166947f2d2bdce7c82c6fda8613c0f6cf
Suhail Razzak <suhail.razzak@gmail.com>
2025-07-30 13:20:38 -0400

    add details for building

--- apps/web/react-router.config.ts
+++ apps/web/react-router.config.ts
@@ -4,6 +4,6 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  ssr: false,
   presets: [vercelPreset()],
 } satisfies Config;
