c1a1b438278473f7aad2e869aacdc28ff9ba2d95
Suhail Razzak <suhail.razzak@gmail.com>
2025-09-06 14:37:22 -0400

    minor changes

--- apps/web/app/components/todo/todo-input.tsx
+++ apps/web/app/components/todo/todo-input.tsx
@@ -1,6 +1,7 @@
+import { useShortcut } from "@/hooks/useShortcut";
 import { cn } from "@/lib/utils";
 import { Input } from "@base-ui-components/react";
-import React from "react";
+import React, { useRef } from "react";
 
 interface TodoInputProps extends React.ComponentProps<typeof Input> {
   completed: boolean;
@@ -15,7 +16,7 @@ export const TodoInput = React.forwardRef<
       ref={ref}
       className={cn(
         "text-foreground flex w-full items-center gap-1 p-1 outline-none transition-colors duration-100",
-        completed ? "text-foreground/50 line-through" : "",
+        completed ? "text-foreground-500/50 line-through decoration-2" : "",
       )}
       placeholder="Add title"
       disabled={completed}
