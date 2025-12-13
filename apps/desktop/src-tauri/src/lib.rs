957fda7c876ebfbf46dde9cd59e4dae44ef01b5f
Suhail Razzak <suhail.razzak@gmail.com>
2025-12-12 19:42:38 -0500

    fix window title bar as diff color

--- apps/desktop/src-tauri/src/lib.rs
+++ apps/desktop/src-tauri/src/lib.rs
@@ -1,7 +1,38 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
+use tauri::{TitleBarStyle, WebviewUrl, WebviewWindowBuilder};
+
 pub fn run() {
     tauri::Builder::default()
         .setup(|app| {
+            let win_builder = WebviewWindowBuilder::new(app, "main", WebviewUrl::default())
+                .title("")
+                .inner_size(800.0, 600.0);
+
+            // set transparent title bar only when building for macOS
+            #[cfg(target_os = "macos")]
+            let win_builder = win_builder.title_bar_style(TitleBarStyle::Transparent);
+
+            let window = win_builder.build().unwrap();
+
+            // set background color only when building for macOS
+            #[cfg(target_os = "macos")]
+            {
+                use cocoa::appkit::{NSColor, NSWindow};
+                use cocoa::base::{id, nil};
+
+                let ns_window = window.ns_window().unwrap() as id;
+                unsafe {
+                    let bg_color = NSColor::colorWithRed_green_blue_alpha_(
+                        nil,
+                        247.0 / 255.0,
+                        239.0 / 255.0,
+                        232.5 / 255.0,
+                        1.0,
+                    );
+                    ns_window.setBackgroundColor_(bg_color);
+                }
+            }
+
             if cfg!(debug_assertions) {
                 app.handle().plugin(
                     tauri_plugin_log::Builder::default()
@@ -9,6 +40,7 @@ pub fn run() {
                         .build(),
                 )?;
             }
+
             Ok(())
         })
         .run(tauri::generate_context!())
